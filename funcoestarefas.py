4d36a8a5f15a19fd40af99232a9b67618d761f86
Matheus <matheuspedrosa307@gmail.com>
2024-07-10 17:38:48 -0300

    ta indo

--- funcoestarefas.py
+++ funcoestarefas.py
@@ -1,18 +1,27 @@
 
 def adicionarTarefa(tarefas, tarefasDic):
-    tarefa = input("Digite o nome da tarefa para adicionar: ")
+    tarefa = input("\nDigite o nome da tarefa para adicionar: ").lower().strip()
     tarefas.append(tarefa)
     tarefasDic.update({tarefa: "Não Concluída"})
+    return print(f"Tarefa '{tarefa}' adicionada a lista de tarefas!")
 
 def removerTarefa(tarefas, tarefasDic):
-    tarefa = input("Digite o nome da tarefa para remover: ")
-    tarefas.remove(tarefa)
-    tarefasDic.pop(tarefa)
+    tarefa = input("\nDigite o nome da tarefa para remover: ").lower().strip()
+    if tarefa in tarefas:
+        tarefas.remove(tarefa)
+        tarefasDic.pop(tarefa)
+        return print(f"Tarefa '{tarefa}' removida da lista de tarefas!")
+    else:
+        return print(f"Tarefa '{tarefa}' não encontrada!")
 
 def checkTarefa(tarefasDic):
-    tarefa = input("Digite o nome da tarefa para marcar como conluída: ")
-    tarefasDic.update({tarefa: "Concluída"})
-    print(tarefasDic)
+    tarefa = input("\nDigite o nome da tarefa para marcar como conluída: ").lower().strip()
+    if tarefa in tarefasDic:
+        tarefasDic.update({tarefa: "Concluída"})
+        return print(f" '{tarefa}' marcada como concluída!")
+    else:
+        return print(f"Tarefa '{tarefa}' não encontrada!")
+    
 
 def listarConcluidas(tarefasDic, concluidas):
     for tarefa, status in tarefasDic.items():
--- gerenciadorDeTarefas.py
+++ gerenciadorDeTarefas.py
@@ -16,32 +16,37 @@ tarefas = []
 tarefasDic = {}
 concluidas = []
 naoConcluidas = []
-opcoesValidas = (1, 2, 3, 4, 5, 6, 7)
+opcoesValidas = ("1", "2", "3", "4", "5", "6", "7")
 sair = False
 
 while sair == False:
-    opcao = int(input("Escolha uma opção: "))
+    opcao = input("\nEscolha uma opção: ").strip()
 
-    if opcao == 1:
-        ft.adicionarTarefa(tarefas, tarefasDic)
-        print(tarefas)
+    if opcao not in opcoesValidas:
+        print("Opção inválida, digite um número entre 1 e 7!\n")
+    else: 
+        opcao = int(opcao)
 
-    if opcao == 2:
-        ft.removerTarefa(tarefas, tarefasDic)
-        print(tarefas)
+        if opcao == 1:
+            ft.adicionarTarefa(tarefas, tarefasDic)
 
-    if opcao == 3:
-        ft.checkTarefa(tarefasDic)
+        if opcao == 2:
+            ft.removerTarefa(tarefas, tarefasDic)
 
-    if opcao == 4:
-        print(tarefas)
-    
-    if opcao == 5:
-        ft.listarConcluidas(tarefasDic, concluidas)
-    
-    if opcao == 6:
-        ft.listarNaoConcluidas(tarefasDic, naoConcluidas)
+        if opcao == 3:
+            ft.checkTarefa(tarefasDic)
 
-    if opcao == 7:
-        print("Programa finalizado!")
-        sair = True
+        if opcao == 4:
+            print('------Minhas Tarefas------')
+            print(tarefas)
+            print('--------------------------')
+        
+        if opcao == 5:
+            ft.listarConcluidas(tarefasDic, concluidas)
+        
+        if opcao == 6:
+            ft.listarNaoConcluidas(tarefasDic, naoConcluidas)
+
+        if opcao == 7:
+            print("Programa finalizado!")
+            sair = True
